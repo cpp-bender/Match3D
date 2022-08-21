64e03e3af88feeef95fbdeee34b70f3ecc593b86
Ahmet Omak <ahmetaltioglu@hotmail.com>
2022-08-21 13:52:21 +0300

    feat(dev): add restriction

--- Assets/GameAssets/Scripts/Selectable.cs
+++ Assets/GameAssets/Scripts/Selectable.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 namespace Match3D
 {
     [RequireComponent(typeof(Collider))]
-	public class Selectable : MonoBehaviour
-	{
+    public class Selectable : MonoBehaviour
+    {
         private Vector3 velocity = Vector3.zero;
 
         [Header("DEPENDENCIES")]
@@ -15,6 +15,7 @@ namespace Match3D
         [Header("DEBUG")]
         public Material selectedMat;
         public Material unSelectedMat;
+        public bool insideSafeArea;
 
         private void OnMouseDown()
         {
@@ -24,9 +25,14 @@ namespace Match3D
 
         private void OnMouseDrag()
         {
-            Vector3 tPos = GetMouseWorldPos() + dragData.Offset;
-            tPos.y = dragData.MaxHeight;
-            transform.position = Vector3.SmoothDamp(transform.position, tPos, ref velocity, dragData.Duration);
+            if (IsInsideSafeArea())
+            {
+                MoveInsideSafeArea();
+            }
+            else
+            {
+                MoveOutsideSafeArea();
+            }
         }
 
         private void OnMouseUp()
@@ -40,6 +46,38 @@ namespace Match3D
             Vector3 sMousePos = Input.mousePosition;
             sMousePos.z = Camera.main.WorldToScreenPoint(transform.position).z;
             return Camera.main.ScreenToWorldPoint(sMousePos);
+        }
+
+        private bool IsInsideSafeArea()
+        {
+            Vector3 wPos = GetMouseWorldPos();
+
+            Vector3 vPos = Camera.main.WorldToViewportPoint(wPos);
+
+            if ((vPos.x >= 0f && vPos.x <= 1f) && (vPos.y >= .1f && vPos.y < .9f))
+            {
+                insideSafeArea = true;
+                return true;
+            }
+
+            insideSafeArea = false;
+            return false;
+        }
+
+        private void MoveInsideSafeArea()
+        {
+            Vector3 tPos = GetMouseWorldPos() + dragData.Offset;
+            tPos.y = dragData.MaxHeight;
+            transform.position = Vector3.SmoothDamp(transform.position, tPos, ref velocity, dragData.Duration);
+        }
+
+        private void MoveOutsideSafeArea()
+        {
+            Vector3 dir = transform.InverseTransformPoint(GetMouseWorldPos()).z <= 0 ? Vector3.back : Vector3.forward;
+
+            Vector3 tPos = GetMouseWorldPos() - (dir);
+            tPos.y = dragData.MaxHeight;
+            transform.position = Vector3.SmoothDamp(transform.position, tPos, ref velocity, dragData.Duration);
         }
     }
 }
